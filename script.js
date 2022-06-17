71fc274860ac386e443f781b9281a14c06f45ec6
Jardaxion <bainthecreator21@gmail.com>
2022-06-18 00:24:06 +0700

    version 1.0

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 //Function for on or off visible options
-function OnOffselect(idSelect, idOption){
-    $(idSelect).toggleClass('active');
-    $(idOption).toggleClass('active');
+function OnOffselect(classSelect, classOption){
+    document.querySelector(classSelect).classList.toggle('active');
+    document.querySelector(classOption).classList.toggle('active');
 }
 
 //Function for replace text from options to headline select
-function replace(idSelect, idOption, idOptions){
-    let select = document.querySelector(idSelect+' p:first-of-type')
-    let option = document.querySelector('.'+idOption)
+function replace(classSelect, classOption, classOptions){
+    let select = document.querySelector(classSelect+' p:first-of-type');
+    let option = document.querySelector('.'+classOption);
     select.innerHTML = option.innerHTML;
-    OnOffselect(idSelect, idOptions);
+    OnOffselect(classSelect, classOptions);
 }
 
 //Function fro first replace text from options to headline select
-function firstReplace(idSelect, idOption){
-    let select = document.querySelector(idSelect+' p:first-of-type')
-    let option = document.querySelector(idOption)
-    select.innerHTML = option.innerHTML;
+function firstReplace(classSelect, classOption){
+    let select = document.querySelector(classSelect+' p:first-of-type')
+    let option = document.querySelector(classOption)
+    select.innerHTML = option.innerHTML
 }
 
-$('.select_body').each(function(){
-    let selectBody = '#'+$(this).attr('id') //We write down Id select (It is necessary to distinguish them from each other)
-    firstReplace(selectBody+' .select', selectBody+' .options .option0')//Aplying frist replace function
+document.querySelectorAll('.select_body').forEach(function(el){
+    let selectBody = '#'+el.getAttribute('id')
+    firstReplace(selectBody+' .select', selectBody+' .options .option0')
 
-    $(selectBody + ' .select').click(function(){ //Set click on header custom select
+    document.querySelector(selectBody + ' .select').addEventListener('click', function(){
         OnOffselect(selectBody + ' .select', selectBody + ' .options')//Aplying on or off function
     })
 
-    $(selectBody + ' .options .option').click(function(){ //Set click on option
-        replace(selectBody + ' .select', $(this).attr('class').replace('option ', ''), selectBody + ' .options') //Aplying replace function
-        $(selectBody + ' select').prop('selectedIndex', this.getAttribute('class').replace('option ', '').replace(/[^\d]/g, '')) //We look at which element we have selected and select the same element in the usual select
+    document.querySelectorAll(selectBody + ' .options .option').forEach(function(option){
+        option.addEventListener('click', function(){
+            replace(selectBody + ' .select', this.getAttribute('class').replace('option ', ''), selectBody + ' .options') //Aplying replace function
+            document.querySelector(selectBody + ' select').selectedIndex = this.getAttribute('class').replace('option ', '').replace(/[^\d]/g, '') //We look at which element we have selected and select the same element in the usual select
+        })
     })
 })
